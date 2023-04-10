39147eb446e7781df88b003ee96e24f21d071907
skadogg <wesanderson@gmail.com>
2023-04-09 20:48:13 -0500

    push script

---
+++ scripts/push.ps1
@@ -0,0 +1,3 @@
+# run as admin
+choco apikey --key 53184e18-4a56-4cfa-a801-df2e8063a778 --source https://push.chocolatey.org/
+choco push C:\Users\gunner\Documents\Git\ghostwriter-portable\ghostwriter.portable.2.1.6.nupkg --source https://push.chocolatey.org/
